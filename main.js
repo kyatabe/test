84b21ea8559c69fabb32415c0848c0d55e3ae803
Kairi Ishizuka <k.ishizuka@increws.co.jp>
2014-08-25 17:49:44 +0900

    add Constructor and some UI outputs

--- main.js
+++ main.js
@@ -7,7 +7,27 @@ process.stdin.setEncoding('utf8');
 // #####################################################################################################################
 
 function Calculator() {
+	// constractor //
+	this.result = 0;
+	this.operator = '';
+	this.value = 0;
 }
+
+Calculator.prototype = {
+	constructor: Calculator,
+	requestToInputValues: function(){
+		requestToInputValues(this.result,this.operator);
+	},
+	requestToSelectOperators: function(){
+		requestToSelectOperators(this.result);
+	},
+	requestToInputFirstValues: function(){
+		requestToInputFirstValues();
+	},
+	outputResult: function(){
+		outputResult(this.result);
+	}
+};
 
 // #####################################################################################################################
 // ############################################## F U N C T I O N S ####################################################
@@ -32,16 +52,58 @@ function readUserInput(cb) {
 	});
 }
 
+/**
+ * UI functions
+ *
+ */
+
+function outputResult(result){
+	console.log('= ' + result);
+	console.log('');
+}
+
+function requestToInputFirstValues() {
+	console.log('<< Please input some numbers... >>');
+	console.log('?');
+	console.log('');
+}
+
+function requestToSelectOperators(result){
+	console.log('<< Please select operators... >>');
+	console.log('+  or  -  or  /  or  *');
+	console.log('');
+	console.log(result + ' ?');
+	console.log('');
+}
+
+function requestToInputValues(result,operator){
+	console.log('<< Please input some numbers... >>');
+	console.log(result + ' ' + operator + ' ?');
+	console.log('');
+}
+
 // #####################################################################################################################
 // ################################################### M A I N #########################################################
 // #####################################################################################################################
 
-var calculator = new Calculator();
+console.log('Starting Javascript Calculator...');
 
-// Remove the following
-// BEGIN
-console.log('What is your name ?');
+// init Calculator
+var calculator = new Calculator();
+var result,operator,value;
+console.log(calculator);
+
+// output test
+
+calculator.requestToInputFirstValues();
+calculator.outputResult();
+calculator.requestToSelectOperators();
+calculator.requestToInputValues();
+
+/*
 readUserInput(function (err, name) {
 	console.log('Your name is ' + name);
 });
+*/
+
 // END
