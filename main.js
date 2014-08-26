600ea12eda95c20c2d8af3c074ab458d6dd0e598
Kairi Ishizuka <k.ishizuka@increws.co.jp>
2014-08-26 15:02:50 +0900

    Fix main.js and package.json

--- main.js
+++ main.js
@@ -1,86 +1,48 @@
 // Encoding for the standard input
 process.stdin.setEncoding('utf8');
 
+// require readline module.
+var rl = require('readline');
 
 // #####################################################################################################################
 // ################################################ C L A S S E S ######################################################
 // #####################################################################################################################
 
 function Calculator() {
-	// constractor //
+	// constructor //
 	this.result = 0;
 	this.operator = '';
 	this.value = 0;
 }
 
-Calculator.prototype = {
-	constructor: Calculator,
-	requestToInputValues: function(){
-		requestToInputValues(this.result,this.operator);
-	},
-	requestToSelectOperators: function(){
-		requestToSelectOperators(this.result);
-	},
-	requestToInputFirstValues: function(){
-		requestToInputFirstValues();
-	},
-	outputResult: function(){
-		outputResult(this.result);
-	}
+Calculator.prototype.requestToInputValues = function(){
+	console.log('<< Please input some numbers... >>');
+	console.log(this.result + ' ' + this.operator + ' ?');
+	console.log('');	
+};
+
+Calculator.prototype.requestToInputFirstValues = function(){
+	console.log('<< Please input some numbers... >>');
+	console.log('?');
+	console.log('');
+};
+
+Calculator.prototype.requestToSelectOperators = function(){
+	console.log('<< Please select operators... >>');
+	console.log('+  or  -  or  /  or  *');
+	console.log('');
+	console.log(this.result + ' ?');
+	console.log('');
+};
+
+Calculator.prototype.outputResult = function(){
+	console.log('= ' + this.result);
+	console.log('');
 };
 
 // #####################################################################################################################
 // ############################################## F U N C T I O N S ####################################################
 // #####################################################################################################################
-
-
-/**
- * @desc Read the standard input.
- *
- * @param  {Function} cb - `cb(err, readData)`
- */
-function readUserInput(cb) {
-	var chunk = '';
-	process.stdin.on('readable', function () {
-		var tmp = process.stdin.read();
-		if (tmp !== null) {
-			chunk += tmp;
-		}
-	});
-	process.stdin.on('end', function () {
-		return cb(null, chunk);
-	});
-}
-
-/**
- * UI functions
- *
- */
-
-function outputResult(result){
-	console.log('= ' + result);
-	console.log('');
-}
-
-function requestToInputFirstValues() {
-	console.log('<< Please input some numbers... >>');
-	console.log('?');
-	console.log('');
-}
-
-function requestToSelectOperators(result){
-	console.log('<< Please select operators... >>');
-	console.log('+  or  -  or  /  or  *');
-	console.log('');
-	console.log(result + ' ?');
-	console.log('');
-}
-
-function requestToInputValues(result,operator){
-	console.log('<< Please input some numbers... >>');
-	console.log(result + ' ' + operator + ' ?');
-	console.log('');
-}
 
 /**
  * Main proccess functions
@@ -96,20 +58,6 @@ function inputValue(cb) {
 	});
 }
 
-/**
- * Test function
- *
- */
-
-function inputByLineTest() {
-	var i = rl.createInterface(process.stdin,process.stdout, null);
-	i.question('This is input test. Please input some words and hit enter key.', function(answer){
-		console.log('You just typed that "' + answer + '"');
-		i.close();
-		process.stdin.destroy();
-	});
-}
-
 // #####################################################################################################################
 // ################################################### M A I N #########################################################
 // #####################################################################################################################
@@ -120,22 +68,6 @@ console.log('Starting Javascript Calculator...');
 var calculator = new Calculator();
 var result,operator,value;
 console.log(calculator);
-
-// require readline module.
-var rl = require('readline');
-
-// output test
-
-//calculator.requestToInputFirstValues();
-//calculator.outputResult();
-//calculator.requestToSelectOperators();
-//calculator.requestToInputValues();
-
-/*
-readUserInput(function (err, name) {
-	console.log('Your name is ' + name);
-});
-*/
 
 // input test
 inputValue(function(err,answer){
